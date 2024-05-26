a539c9a2cab6d427a447027f17d577a231277862
ysdayone <166668156+ysdayone@users.noreply.github.com>
2024-05-26 09:06:04 +0900

    kadai_mds再アップロード

--- kadai_mdn_.js/mdn.js
+++ kadai_mdn_.js/mdn.js
@@ -2,4 +2,4 @@
 const date = new Date();
 
 //日付の関数を呼び出す
-console.log(date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日');
+console.log(date.getFullYear() + '年' + (date.getMonth()+1) + '月' + date.getDate() + '日');
